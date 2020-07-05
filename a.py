7435ddb090a6cb66757616ab47df1d412fbdc8da
Edward Kan <kan1010@freenet.de>
2020-07-05 20:21:43 +0200

    selenium example ok

--- a.py
+++ a.py
@@ -14,5 +14,12 @@ def test2():
     browser = webdriver.Firefox()
     print "Webdriver object", browser
     browser.get('https://facebook.com')
+    email = browser.find_element_by_name('email')
+    pwd = browser.find_element_by_name('pass')
+    print "Html elements:"
+    print "Email:", email, "\npassword:", pwd
+    email.send_keys('edkan1@gmail.com')
+    pwd.send_keys('5!dearms')
+    browser.find_element_by_id('loginbutton').click()
 
 test2()
