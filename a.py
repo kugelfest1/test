b1a654d10a1b4d528686c89a03507f6f0329d084
Edward Kan <kan1010@freenet.de>
2020-07-05 20:08:43 +0200

    selenium example

--- a.py
+++ a.py
@@ -6,4 +6,13 @@ def test():
     print("Response object:", r)
     print("Response text:", r.text)
 
-test()
+""" example using selenoum
+    remember to install selenium, and download geckodriver
+"""
+from selenium import webdriver
+def test2():
+    browser = webdriver.Firefox()
+    print "Webdriver object", browser
+    browser.get('https://facebook.com')
+
+test2()
